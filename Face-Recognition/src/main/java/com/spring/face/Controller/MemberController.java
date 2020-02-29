cc569ca9e766fab792a549f2ac0c68c22a4a04f3
SeonSangWon <sangwon7482@naver.com>
2020-02-29 18:13:13 +0900

    메인페이지, 회원가입 페이지 디자인 완료 / 회원가입 로직 구현

--- Face-Recognition/src/main/java/com/spring/face/Controller/MemberController.java
+++ Face-Recognition/src/main/java/com/spring/face/Controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.spring.face.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,36 @@ public class MemberController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
+	@RequestMapping("signup")
+	public String signUp() {
+		
+		return "member/signup";
+	}
+	
+	@RequestMapping("signin")
+	public String signIn() {
+		
+		return "home";
+	}
+	
 	//회원 : 정보 저장
 	@RequestMapping(value="memberInsert", method=RequestMethod.POST)
-	public String memberInsert(MemberDTO memberDTO, Model model) {
+	public String memberInsert(MemberDTO memberDTO, Model model, HttpServletRequest request) {
 		
+		//초기화
+		String phone_1 = "";
+		String phone_2 = "";
 		try {
+			phone_1 = request.getParameter("phone_1");
+			phone_2 = request.getParameter("phone_2");
+			memberDTO.setPhone(phone_1 + phone_2);
+			
+			logger.debug("학번 : " + memberDTO.getStudent_id());
+			logger.debug("이름 : " + memberDTO.getName());
+			logger.debug("아이디 : " + memberDTO.getId());
+			logger.debug("비밀번호 : " + memberDTO.getPassword());
+			logger.debug("전화번호 : " + memberDTO.getPhone());
+			
 			//회원가입
 			memberService.memberInsert(memberDTO);
 			logger.debug("MemberController : memberInsert() Success!!");
