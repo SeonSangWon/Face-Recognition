8ade0edd1e8a274e9485b39dc166a8bda50011c1
SeonSangWon <sangwon7482@naver.com>
2020-03-02 21:30:22 +0900

    충돌로 인한 프로젝트 삭제

--- Face-Recognition/src/main/java/com/spring/face/Util/IParsing.java
+++
@@ -1,9 +0,0 @@
-package com.spring.face.Util;
-
-public interface IParsing {
-
-	//종합정보 로그인
-	public void TotalInfomation(String userId, String password);
-	//사이버강의실 로그인
-	public void CyberRoom(String userId, String password);
-}
